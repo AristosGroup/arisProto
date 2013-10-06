dba2cc95616ace7981dda87f77b2a45bff53de41
Aleksey Kuznetsov <mrakobesov@gmail.com>
2013-10-07 00:18:14 +0400

    remove old files

--- newtest/js/controllers/new_issue_controller.js
+++
@@ -1,7 +0,0 @@
-var EditIssueController = require('./edit_issue_controller');
-
-// inherit from edit controller
-var NewIssueController = EditIssueController.extend();
-
-module.exports = NewIssueController;
-
