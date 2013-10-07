f20122dda86b4a1d807b8a32f1af4c47410cc3ec
Aleksey Kuznetsov <mrakobesov@gmail.com>
2013-10-07 19:08:56 +0400

    location history

--- app/router.js
+++ app/router.js
@@ -1,5 +1,10 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
+Router.reopen({
+    location: 'history'
+});
+
+
 Router.map(function(){
     this.resource('issues', { path: '/issues' }, function() {
         this.resource('issue', { path: '/issue/:issue_id' });
