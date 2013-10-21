f47996b63bbf869e955c57dfcc346e27a1bfc7e3
Aleksey Kuznetsov <mrakobesov@gmail.com>
2013-10-21 19:37:27 +0400

    multi select

--- app/views/tags.js
+++ app/views/tags.js
@@ -1,41 +1,21 @@
 module.exports =  App.TagsView = Ember.Select.extend({
 
-/*
+
     didInsertElement: function() {
         Ember.run.scheduleOnce('afterRender', this, 'processChildElements');
     },
 
     processChildElements: function() {
-        var preload_data = this.get('_parentView.controller').get('alltags');
-    *//*    this.get('_parentView.controller').get('alltags').then(function(tags) {
-            console.log(tags);
-        });*//*
-        console.log(preload_data);
+
+
         this.$().select2({
-            width:'100%',
-            multiple: true
-            ,query: function (query){
-                var data = {results: []};
-
-                preload_data.forEach(function(item, index, enumerable){
-
-
-                    if(query.term.length == 0 || this.text.toUpperCase().indexOf(query.term.toUpperCase()) >= 0 ){
-                        data.results.push({id: item.get('id'), text: item.get('title') });
-                    }
-
-                });
-
-
-
-                query.callback(data);
-            }
+            width:'100%'
         });
     },
 
     willDestroyElement: function () {
         this.$().select2("destroy");
-    }*/
+    }
 
 
 
